ee3002955c798c9139cabe2de45de5d4d3b82f6d
jicjjang <jicjjang12@gmail.com>
2015-05-02 16:20:59 +0900

    add Request to src

---
+++ src/Request.php
@@ -0,0 +1,54 @@
+<?php
+namespace June;
+
+use June\Request\Pattern;
+
+class Request
+{
+    protected $data;
+
+    protected $pattern;
+
+    public function __construct(Array $attribute)
+    {
+        $this->data = $attribute;
+        $this->pattern = new Pattern($this->data['path']);
+    }
+
+    public function getPath()
+    {
+        return $this->data['path'];
+    }
+
+    public function getUri()
+    {
+        if (!isset($this->data['uri'])) {
+            $this->data['uri'] = $this->pattern->parseUri();
+        }
+        return $this->data['uri'];
+    }
+
+    public function getArgs()
+    {
+        if (!isset($this->data['args'])) {
+            $this->data['args'] = $this->pattern->getArgs();
+        }
+        return $this->data['args'];
+    }
+
+    public function getPattern()
+    {
+        if (!isset($this->data['pattern'])) {
+            $this->data['pattern'] = $this->pattern->getPattern();
+        }
+        return $this->data['pattern'];
+    }
+
+    public function getMethod()
+    {
+//        $args = $this->pattern->getArgs();
+//        return $this->data['method'];
+    }
+
+    // getBody, getHeaders, getQueries, getParameters, getHeader, getQuery, getParameter
+}
---
+++ tests/src/RequestTest.php
@@ -0,0 +1,65 @@
+<?php
+namespace June;
+
+use PHPUnit_Framework_TestCase;
+
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+    protected $request;
+
+    public function setUp()
+    {
+        $this->request[0] = new Request([
+            'method'=> 'GET',
+            'path'=>'/'
+        ]);
+        $this->request[1] = new Request([
+            'method'=> 'GET',
+            'path'=> '/test/'
+        ]);
+        $this->request[2] = new Request([
+            'method'=> 'GET',
+            'path'=> '/test?value=1&asdf=3'
+        ]);
+        $this->request[3] = new Request([
+            'method'=> 'GET',
+            'path'=> '/test;param;param?value=1&asdf=4'
+        ]);
+        $this->request[4] = new Request([
+            'method' => 'POST',
+            'path' => '/test/{id}'
+        ]);
+        $this->request[5] = new Request([
+            'method' => 'POST',
+            'path' => '/test/{page}/{num}'
+        ]);
+    }
+
+    public function testGetUri()
+    {
+        $this->assertEquals('', $this->request[0]->getUri());
+        $this->assertEquals('test', $this->request[1]->getUri());
+        $this->assertEquals('test', $this->request[2]->getUri());
+        $this->assertEquals('test', $this->request[3]->getUri());
+    }
+
+    public function testGetArgs()
+    {
+        $this->assertEquals(array(), $this->request[0]->getArgs());
+        $this->assertEquals(array(), $this->request[1]->getArgs());
+        $this->assertEquals(array(), $this->request[2]->getArgs());
+        $this->assertEquals(array(), $this->request[3]->getArgs());
+        $this->assertEquals(array('id'), $this->request[4]->getArgs());
+        $this->assertEquals(array('page', 'num'), $this->request[5]->getArgs());
+    }
+
+    public function testGetPattern()
+    {
+        $this->assertEquals('', $this->request[0]->getPattern());
+        $this->assertEquals('test', $this->request[1]->getPattern());
+        $this->assertEquals('test', $this->request[2]->getPattern());
+        $this->assertEquals('test', $this->request[3]->getPattern());
+        $this->assertEquals("test\/(?<id>[^\/\#]+)", $this->request[4]->getPattern());
+        $this->assertEquals("test\/(?<page>[^\/\#]+)\/(?<num>[^\/\#]+)", $this->request[5]->getPattern());
+    }
+}
