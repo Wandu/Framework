8c24f1e06bc2744dc940731b6b5a69e01153355f
Changwan Jun <me@wani.kr>
2015-05-12 03:46:10 +0900

    change Exceptions __construct

--- src/CannotChangeException.php
+++ src/CannotChangeException.php
@@ -1,9 +1,13 @@
 <?php
 namespace Wandu\DI;
 
+use Exception;
 use RuntimeException;
 
 class CannotChangeException extends RuntimeException
 {
-    protected $message = 'You cannot change the data.';
+    public function __construct($message = 'You cannot change the data.', $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
--- src/NullReferenceException.php
+++ src/NullReferenceException.php
@@ -1,9 +1,17 @@
 <?php
 namespace Wandu\DI;
 
+use Exception;
 use RuntimeException;
 
 class NullReferenceException extends RuntimeException
 {
-    protected $message = 'You cannot access null reference container.';
+    public function __construct(
+        $message = 'You cannot access null reference container.',
+        $code = 0,
+        Exception $previous = null
+    )
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
