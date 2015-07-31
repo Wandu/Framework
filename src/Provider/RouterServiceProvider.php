6b5e338c1ced774b0184d791f8e61ae23793790d
Changwan Jun <me@wani.kr>
2015-07-31 12:28:27 +0900

    add RouterServiceProvider

--- src/Provider/RouterServiceProvider.php
+++ src/Provider/RouterServiceProvider.php
@@ -4,7 +4,9 @@ namespace Wandu\Router\Provider;
 use ArrayAccess;
 use Wandu\DI\ContainerInterface;
 use Wandu\DI\ServiceProviderInterface;
-use Wandu\Router\Middleware\MapperInterface;
+use Wandu\Router\Mapper\WanduMapper;
+use Wandu\Router\MapperInterface;
+use Wandu\Router\Router;
 
 class RouterServiceProvider implements ServiceProviderInterface
 {
@@ -15,8 +17,10 @@ class RouterServiceProvider implements ServiceProviderInterface
      */
     public function register(ContainerInterface $app, ArrayAccess $config = null)
     {
-        $app->closure(MapperInterface::class, function () {
-
+        $app->closure(MapperInterface::class, function (ContainerInterface $app) use ($config) {
+            return new WanduMapper($app, $config['router.namespace.handler'], $config['router.namespace.middleware']);
         });
+        $app->bind(Router::class);
+        $app->alias('wandu.router', Router::class);
     }
 }
