596b86c081ba5197fb7d6af60456ae03dd5e2c81
Changwan Jun <im@wani.kr>
2016-02-15 01:48:44 +0900

    add fastelper to composer.json

---
+++ src/Token.php
@@ -0,0 +1,35 @@
+<?php
+namespace Wandu\Compiler;
+
+class Token
+{
+    protected $name;
+
+    protected $value;
+
+    /**
+     * @param mixed $name
+     * @param mixed $value
+     */
+    public function __construct($name, $value = null)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
