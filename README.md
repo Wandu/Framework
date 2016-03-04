290610b5bd20dc19a1d44bfdb454ca6ee30d4cdc
Changwan Jun <im@wani.kr>
2016-03-04 16:52:03 +0900

    first commit from Wandu/Http

---
+++ README.md
@@ -0,0 +1,5 @@
+Wandu Caster
+===
+
+Perfect Type Casting Library.
+
---
+++ tests/CasterTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Wandu\Caster;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+class CasterTest extends PHPUnit_Framework_TestCase
+{
+    use CastProviderTrait;
+
+    /**
+     * @dataProvider castingProvider
+     */
+    public function testCasting($input, $cast, $output)
+    {
+        if (is_object($output)) {
+            $this->assertInstanceOf(\stdClass::class, (new Caster($input))->cast($cast));
+            $this->assertEquals($output, (new Caster($input))->cast($cast));
+        } else {
+            $this->assertSame($output, (new Caster($input))->cast($cast));
+        }
+    }
+}
