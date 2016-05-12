568422b9f60854cf8576c8bc6f55b75f3150f836
Changwan Jun <im@wani.kr>
2016-05-12 16:23:00 +0900

    fix composer.json and split builder

---
+++ build/split-full.sh
@@ -0,0 +1,14 @@
+
+TAGS="v3.0.0-alpha2"
+
+git subsplit init git@github.com:Wandu/Framework.git
+
+git subsplit publish --heads="master" --tags=$TAGS src/Wandu/Caster:git@github.com:Wandu/Caster.git
+git subsplit publish --heads="master" --tags=$TAGS src/Wandu/Compiler:git@github.com:Wandu/Compiler.git
+git subsplit publish --heads="master" --tags=$TAGS src/Wandu/DI:git@github.com:Wandu/DI.git
+git subsplit publish --heads="master" --tags=$TAGS src/Wandu/Http:git@github.com:Wandu/Http.git
+git subsplit publish --heads="master" --tags=$TAGS src/Wandu/Q:git@github.com:Wandu/Q.git
+git subsplit publish --heads="master" --tags=$TAGS src/Wandu/Router:git@github.com:Wandu/Router.git
+git subsplit publish --heads="master" --tags=$TAGS src/Wandu/Tempy:git@github.com:Wandu/Tempy.git
+
+rm -rf .subsplit/
--- build/split.sh
+++ build/split.sh
@@ -1,6 +1,12 @@
+
 git subsplit init git@github.com:Wandu/Framework.git
-git subsplit publish --heads="master" src/Wandu/DI:git@github.com:Wandu/DI.git
-git subsplit publish --heads="master" src/Wandu/Http:git@github.com:Wandu/Http.git
-git subsplit publish --heads="master" src/Wandu/Router:git@github.com:Wandu/Router.git
+
+git subsplit publish --heads="master" --no-tags src/Wandu/Caster:git@github.com:Wandu/Caster.git
+git subsplit publish --heads="master" --no-tags src/Wandu/Compiler:git@github.com:Wandu/Compiler.git
+git subsplit publish --heads="master" --no-tags src/Wandu/DI:git@github.com:Wandu/DI.git
+git subsplit publish --heads="master" --no-tags src/Wandu/Http:git@github.com:Wandu/Http.git
 git subsplit publish --heads="master" --no-tags src/Wandu/Q:git@github.com:Wandu/Q.git
+git subsplit publish --heads="master" --no-tags src/Wandu/Router:git@github.com:Wandu/Router.git
+git subsplit publish --heads="master" --no-tags src/Wandu/Tempy:git@github.com:Wandu/Tempy.git
+
 rm -rf .subsplit/
