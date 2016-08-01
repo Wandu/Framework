d8e251bbece3e75cac487993d69ef5679af5f7b0
Changwan Jun <im@wani.kr>
2016-08-01 18:10:48 +0900

    split Config into Config and DotArray @wandu/support

--- src/Wandu/Config/Config.php
+++ src/Wandu/Config/Config.php
@@ -3,54 +3,23 @@ namespace Wandu\Config;
 
 use Wandu\Config\Contracts\ConfigInterface;
 use Wandu\Config\Exception\NotAllowedMethodException;
-use InvalidArgumentException;
+use Wandu\Support\DotArray;
 
-class Config implements ConfigInterface
+class Config extends DotArray implements ConfigInterface
 {
-    /** @var array */
-    protected $dataSet;
-
     /** @var bool */
     protected $readOnly;
 
     /**
-     * @param array $dataSet
+     * @param array $items
      * @param bool $readOnly
      */
-    public function __construct(array $dataSet, $readOnly = true)
+    public function __construct(array $items = [], $readOnly = true)
     {
-        $this->dataSet = $dataSet;
+        parent::__construct($items);
         $this->readOnly = $readOnly;
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRawData()
-    {
-        return $this->dataSet;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function get($name, $default = null)
-    {
-        if ($name === '') {
-            return $this->dataSet;
-        }
-        $names = explode('.', $name);
-        $dataToReturn = $this->dataSet;
-        while (count($names)) {
-            $name = array_shift($names);
-            if (!is_array($dataToReturn) || !array_key_exists($name, $dataToReturn)) {
-                return $default;
-            }
-            $dataToReturn = $dataToReturn[$name];
-        }
-        return $dataToReturn;
-    }
-
+    
     /**
      * {@inheritdoc}
      */
@@ -59,42 +28,7 @@ class Config implements ConfigInterface
         if ($this->readOnly) {
             throw new NotAllowedMethodException();
         }
-        if ($name === '') {
-            $this->dataSet = $value;
-        }
-        $names = explode('.', $name);
-        $dataToSet = &$this->dataSet;
-        while (count($names)) {
-            $name = array_shift($names);
-            if (!is_array($dataToSet)) {
-                $dataToSet = [];
-            }
-            if (!array_key_exists($name, $dataToSet)) {
-                $dataToSet[$name] = null;
-            }
-            $dataToSet = &$dataToSet[$name];
-        }
-        $dataToSet = $value;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function has($name)
-    {
-        if ($name === '') {
-            return true;
-        }
-        $names = explode('.', $name);
-        $dataToReturn = $this->dataSet;
-        while (count($names)) {
-            $name = array_shift($names);
-            if (!is_array($dataToReturn) || !array_key_exists($name, $dataToReturn)) {
-                return false;
-            }
-            $dataToReturn = $dataToReturn[$name];
-        }
-        return true;
+        return parent::set($name, $value);
     }
 
     /**
@@ -105,71 +39,6 @@ class Config implements ConfigInterface
         if ($this->readOnly) {
             throw new NotAllowedMethodException();
         }
-        if ($name === '') {
-            $this->dataSet = [];
-            return true;
-        }
-        $names = explode('.', $name);
-        $dataToReturn = &$this->dataSet;
-        while (count($names)) {
-            $name = array_shift($names);
-            if (!is_array($dataToReturn) || !array_key_exists($name, $dataToReturn)) {
-                return false;
-            }
-            if (count($names) === 0) {
-                unset($dataToReturn[$name]);
-            } else {
-                $dataToReturn = &$dataToReturn[$name];
-            }
-        }
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function subset($name)
-    {
-        $subset = $this->get($name);
-        if (!is_array($subset)) {
-            throw new InvalidArgumentException('subset must be an array.');
-        }
-        return new static($subset);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetExists($offset)
-    {
-        return $this->has($offset);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetGet($offset)
-    {
-        if (strpos($offset, '||') !== false) {
-            list($offset, $default) = explode('||', $offset);
-            return $this->get($offset, $default);
-        }
-        return $this->get($offset);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetSet($offset, $value)
-    {
-        $this->set($offset, $value);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetUnset($offset)
-    {
-        $this->remove($offset);
+        return parent::remove($name);
     }
 }
---
+++ src/Wandu/Support/DotArray.php
@@ -0,0 +1,163 @@
+<?php
+namespace Wandu\Support;
+
+use InvalidArgumentException;
+use ArrayAccess;
+
+class DotArray implements ArrayAccess
+{
+    /** @var array */
+    protected $items;
+
+    /**
+     * @param array $items
+     */
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRawData()
+    {
+        return $this->items;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($name, $default = null)
+    {
+        if ($name === '') {
+            return $this->items;
+        }
+        $names = explode('.', $name);
+        $dataToReturn = $this->items;
+        while (count($names)) {
+            $name = array_shift($names);
+            if (!is_array($dataToReturn) || !array_key_exists($name, $dataToReturn)) {
+                return $default;
+            }
+            $dataToReturn = $dataToReturn[$name];
+        }
+        return $dataToReturn;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($name, $value)
+    {
+        if ($name === '') {
+            $this->items = $value;
+        }
+        $names = explode('.', $name);
+        $dataToSet = &$this->items;
+        while (count($names)) {
+            $name = array_shift($names);
+            if (!is_array($dataToSet)) {
+                $dataToSet = [];
+            }
+            if (!array_key_exists($name, $dataToSet)) {
+                $dataToSet[$name] = null;
+            }
+            $dataToSet = &$dataToSet[$name];
+        }
+        $dataToSet = $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($name)
+    {
+        if ($name === '') {
+            return true;
+        }
+        $names = explode('.', $name);
+        $dataToReturn = $this->items;
+        while (count($names)) {
+            $name = array_shift($names);
+            if (!is_array($dataToReturn) || !array_key_exists($name, $dataToReturn)) {
+                return false;
+            }
+            $dataToReturn = $dataToReturn[$name];
+        }
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($name)
+    {
+        if ($name === '') {
+            $this->items = [];
+            return true;
+        }
+        $names = explode('.', $name);
+        $dataToReturn = &$this->items;
+        while (count($names)) {
+            $name = array_shift($names);
+            if (!is_array($dataToReturn) || !array_key_exists($name, $dataToReturn)) {
+                return false;
+            }
+            if (count($names) === 0) {
+                unset($dataToReturn[$name]);
+            } else {
+                $dataToReturn = &$dataToReturn[$name];
+            }
+        }
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function subset($name)
+    {
+        $subset = $this->get($name);
+        if (!is_array($subset)) {
+            throw new InvalidArgumentException('subset must be an array.');
+        }
+        return new static($subset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        if (strpos($offset, '||') !== false) {
+            list($offset, $default) = explode('||', $offset);
+            return $this->get($offset, $default);
+        }
+        return $this->get($offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->set($offset, $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        $this->remove($offset);
+    }    
+}
