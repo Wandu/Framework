de649d98263ab8b0cdd60d57e657ded31a173eea
Changwan Jun <im@wani.kr>
2016-08-15 23:51:24 +0900

    fix model

--- src/Wandu/Database/Modelr/Model.php
+++ src/Wandu/Database/Modelr/Model.php
@@ -3,12 +3,12 @@ namespace Wandu\Database\Modelr;
 
 use ArrayAccess;
 
-abstract class Model implements ArrayAccess
+class Model implements ArrayAccess
 {
     /** @var array */
     protected static $defaults = [];
     
-    /** @var array */
+    /** @var array raw data from database */
     protected $attributes = [];
 
     /**
@@ -17,5 +17,37 @@ abstract class Model implements ArrayAccess
     public function __construct(array $attributes = [])
     {
         $this->attributes = $attributes;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->attributes);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        // TODO: Implement offsetGet() method.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        // TODO: Implement offsetSet() method.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        // TODO: Implement offsetUnset() method.
     }
 }
