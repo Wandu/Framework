c3e196692e875c7c9e3794cac2d914970c575575
Changwan Jun <im@wani.kr>
2016-09-02 18:25:11 +0900

    add ServerParams @wandu/http

--- tests/Http/Psr/MessageTestTrait.php
+++ tests/Http/Psr/MessageTestTrait.php
@@ -15,14 +15,14 @@ trait MessageTestTrait
         $message = $this->message->withProtocolVersion('1.0');
 
         // The string MUST contain only the HTTP version number (e.g., "1.1", "1.0").
-        $this->assertEquals("1.0", $message->getProtocolVersion());
+        static::assertEquals("1.0", $message->getProtocolVersion());
 
         // This method MUST be implemented in such a way as to retain the
         // immutability of the message, and MUST return an instance that has the
         // new protocol version.
-        $this->assertNotSame($message, $message->withProtocolVersion("1.0"));
+        static::assertNotSame($message, $message->withProtocolVersion("1.0"));
 
-        $this->assertEquals("1.1", $message->withProtocolVersion("1.1")->getProtocolVersion());
+        static::assertEquals("1.1", $message->withProtocolVersion("1.1")->getProtocolVersion());
     }
 
     public function testGetHeaders()
@@ -48,9 +48,9 @@ trait MessageTestTrait
 
         //While header names are not case-sensitive, getHeaders() will preserve the
         //exact case in which headers were originally specified.
-        $this->assertEquals([], $message->getHeaders());
+        static::assertEquals([], $message->getHeaders());
 
-        $this->assertEquals(['Test' => ['Hello', 'World!']], $messageWithHeader->getHeaders());
+        static::assertEquals(['Test' => ['Hello', 'World!']], $messageWithHeader->getHeaders());
     }
 
     public function testHasHeader()
@@ -60,10 +60,10 @@ trait MessageTestTrait
             ->withHeader('Test', ['Hello', 'World!']);
 
         // Checks if a header exists by the given case-insensitive name.
-        $this->assertTrue($messageWithHeader->hasHeader('test'));
-        $this->assertTrue($messageWithHeader->hasHeader('teST'));
+        static::assertTrue($messageWithHeader->hasHeader('test'));
+        static::assertTrue($messageWithHeader->hasHeader('teST'));
 
-        $this->assertFalse($messageWithHeader->hasHeader('unknown'));
+        static::assertFalse($messageWithHeader->hasHeader('unknown'));
     }
 
     public function testGetHeader()
@@ -74,12 +74,12 @@ trait MessageTestTrait
 
         // This method returns an array of all the header values of the given
         // case-insensitive header name.
-        $this->assertEquals(['Hello', 'World!'], $messageWithHeader->getHeader('test'));
-        $this->assertEquals(['Hello', 'World!'], $messageWithHeader->getHeader('tESt'));
+        static::assertEquals(['Hello', 'World!'], $messageWithHeader->getHeader('test'));
+        static::assertEquals(['Hello', 'World!'], $messageWithHeader->getHeader('tESt'));
 
         // If the header does not appear in the message, this method MUST return an
         // empty array.
-        $this->assertEquals([], $messageWithHeader->getHeader('unknown'));
+        static::assertEquals([], $messageWithHeader->getHeader('unknown'));
     }
 
     public function testGetHeaderLine()
@@ -91,8 +91,8 @@ trait MessageTestTrait
         // This method returns all of the header values of the given
         // case-insensitive header name as a string concatenated together using
         // a comma.
-        $this->assertEquals('Hello,World!', $messageWithHeader->getHeaderLine('test'));
-        $this->assertEquals('Hello,World!', $messageWithHeader->getHeaderLine('tESt'));
+        static::assertEquals('Hello,World!', $messageWithHeader->getHeaderLine('test'));
+        static::assertEquals('Hello,World!', $messageWithHeader->getHeaderLine('tESt'));
 
         // NOTE: Not all header values may be appropriately represented using
         // comma concatenation. For such headers, use getHeader() instead
@@ -100,7 +100,7 @@ trait MessageTestTrait
 
         // If the header does not appear in the message, this method MUST return
         // an empty string.
-        $this->assertEquals('', $messageWithHeader->getHeaderLine('unknown'));
+        static::assertEquals('', $messageWithHeader->getHeaderLine('unknown'));
     }
 
     public function testWithHeader()
@@ -111,34 +111,51 @@ trait MessageTestTrait
 
         // While header names are case-insensitive, the casing of the header will
         // be preserved by this function, and returned from getHeaders().
-        $this->assertNotSame($messageWithHeader, $messageWithHeader->withHeader('test', 'blabla'));
+        static::assertNotSame($messageWithHeader, $messageWithHeader->withHeader('test', 'blabla'));
 
         try {
             $messageWithHeader->withHeader('test', [[], 'un..']);
             $this->fail();
         } catch (InvalidArgumentException $e) {
-            $this->assertEquals('Invalid header value. It must be a string or array of strings.', $e->getMessage());
+            static::assertEquals('Invalid header value. It must be a string or array of strings.', $e->getMessage());
         }
         // This method MUST be implemented in such a way as to retain the
         // immutability of the message, and MUST return an instance that has the
         // new and/or updated header and value.
 
-        $this->assertEquals(['blabla'], $messageWithHeader->withHeader('other', 'blabla')->getHeader('other'));
-        $this->assertEquals(
+        static::assertEquals(['blabla'], $messageWithHeader->withHeader('other', 'blabla')->getHeader('other'));
+        static::assertEquals(
             ['blabla', 'what'],
             $messageWithHeader->withHeader('other', ['blabla', 'what'])->getHeader('other')
         );
 
-        $this->assertEquals(
+        static::assertEquals(
             ['Hello', 'World!'],
             $messageWithHeader->withHeader('other', 'blabla')->getHeader('test')
         );
 
-        $this->assertEquals(['blabla'], $messageWithHeader->withHeader('test', 'blabla')->getHeader('test'));
-        $this->assertEquals(
+        static::assertEquals(['blabla'], $messageWithHeader->withHeader('test', 'blabla')->getHeader('test'));
+        static::assertEquals(
             ['blabla', 'what'],
             $messageWithHeader->withHeader('test', ['blabla', 'what'])->getHeader('test')
         );
+
+        // overwrap.
+        static::assertEquals([
+            'abc',
+        ], $messageWithHeader->withHeader('test', 'blabla')->withHeader('test', 'abc')->getHeader('test'));
+
+        static::assertEquals([
+            'text/html',
+            'application/xhtml+xml',
+            'application/xml;q=0.9',
+            'image/webp',
+            '*/*;q=0.8',
+        ], $messageWithHeader->withHeader('accept', 'text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, */*;q=0.8')->getHeader('accept'));
+
+        static::assertEquals([
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+        ], $messageWithHeader->withHeader('USER-AGENT', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36')->getHeader('user-agent'));
     }
 
     public function testWithAddedHeader()
@@ -150,17 +167,17 @@ trait MessageTestTrait
         // Existing values for the specified header will be maintained. The new
         // value(s) will be appended to the existing list. If the header did not
         // exist previously, it will be added.
-        $this->assertNotSame($messageWithHeader, $messageWithHeader->withAddedHeader('test', 'blabla'));
+        static::assertNotSame($messageWithHeader, $messageWithHeader->withAddedHeader('test', 'blabla'));
 
         // This method MUST be implemented in such a way as to retain the
         // immutability of the message, and MUST return an instance that has the
         // new header and/or value.
-        $this->assertEquals(
+        static::assertEquals(
             ['Hello', 'World!', 'blabla'],
             $messageWithHeader->withAddedHeader('test', 'blabla')->getHeader('test')
         );
 
-        $this->assertEquals([
+        static::assertEquals([
             'Test' => ['Hello', 'World!'],
             'other' => ['blabla', 'what']
         ], $messageWithHeader->withAddedHeader('other', ['blabla', 'what'])->getHeaders());
@@ -173,16 +190,16 @@ trait MessageTestTrait
             ->withHeader('Test', ['Hello', 'World!']);
 
         // Header resolution MUST be done without case-sensitivity.
-        $this->assertNotSame($messageWithHeader, $messageWithHeader->withoutHeader('foo'));
+        static::assertNotSame($messageWithHeader, $messageWithHeader->withoutHeader('foo'));
 
         // This method MUST be implemented in such a way as to retain the
         // immutability of the message, and MUST return an instance that removes
         // the named header.
-        $this->assertEquals([
+        static::assertEquals([
             'Test' => ['Hello', 'World!']
         ], $messageWithHeader->withoutHeader('other')->getHeaders());
 
-        $this->assertEquals([
+        static::assertEquals([
         ], $messageWithHeader->withoutHeader('test')->getHeaders());
     }
 
@@ -191,15 +208,15 @@ trait MessageTestTrait
         $mockBody = Mockery::mock(StreamInterface::class);
         $messageWithBody = $this->message->withBody($mockBody);
 
-        $this->assertNull($this->message->getBody()); // return null
+        static::assertNull($this->message->getBody()); // return null
 
         // The body MUST be a StreamInterface object.
-        $this->assertNotSame($this->message, $messageWithBody);
+        static::assertNotSame($this->message, $messageWithBody);
 
         // This method MUST be implemented in such a way as to retain the
         // immutability of the message, and MUST return a new instance that has the
         // new body stream.
-        $this->assertInstanceOf(StreamInterface::class, $messageWithBody->getBody());
-        $this->assertSame($mockBody, $messageWithBody->getBody());
+        static::assertInstanceOf(StreamInterface::class, $messageWithBody->getBody());
+        static::assertSame($mockBody, $messageWithBody->getBody());
     }
 }
