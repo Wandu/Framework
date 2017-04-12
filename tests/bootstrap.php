986c0cde77ac36299826890aebf3d760bcd06d75
Changwan Jun <im@wani.kr>
2017-04-13 00:34:13 +0900

    add AnnotationRegistry for test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Wandu\Foundation\Application;
 use Wandu\Foundation\Kernels\NullKernel;
+
+AnnotationRegistry::registerLoader('class_exists');
 
 $app = new Application(new NullKernel());
 $app->execute();
