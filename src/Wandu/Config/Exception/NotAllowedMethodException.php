944328b84993115619b8d124c562a23458af3761
Changwan Jun <im@wani.kr>
2017-06-08 12:20:02 +0900

    fix Exception in config, wrong dependency @wandu/config

--- src/Wandu/Config/Exception/NotAllowedMethodException.php
+++ src/Wandu/Config/Exception/NotAllowedMethodException.php
@@ -1,8 +1,44 @@
 <?php
 namespace Wandu\Config\Exception;
 
-use Wandu\Support\Exception\CannotCallMethodException;
+use RuntimeException;
 
-class NotAllowedMethodException extends CannotCallMethodException
+class NotAllowedMethodException extends RuntimeException
 {
+    /** @var string */
+    protected $methodName;
+
+    /** @var string */
+    protected $className;
+
+    /**
+     * @param string $methodName
+     * @param string $className
+     */
+    public function __construct($methodName, $className = null)
+    {
+        $this->methodName = $methodName;
+        $this->className = $className;
+        $message = "cannot call {$methodName}";
+        if ($className) {
+            $message .= " in {$className}";
+        }
+        parent::__construct($message);
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethodName()
+    {
+        return $this->methodName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->className;
+    }
 }
