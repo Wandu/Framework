1b993ce3a113abf435ece7bac0eba0d019436e6a
Changwan Jun <im@wani.kr>
2017-06-12 11:59:51 +0900

    now you can change application name and version @wandu/console

--- src/Wandu/Console/ConsoleServiceProvider.php
+++ src/Wandu/Console/ConsoleServiceProvider.php
@@ -8,16 +8,19 @@ use Wandu\Foundation\Application;
 
 class ConsoleServiceProvider implements ServiceProviderInterface 
 {
+    /** @var string */
+    protected $name = Application::NAME;
+    
+    /** @var string */
+    protected $version = Application::VERSION;
+    
     /** @var array */
     protected $commands = [];
     
     public function register(ContainerInterface $app)
     {
         $app->closure(SymfonyApplication::class, function () {
-            return new SymfonyApplication(
-                Application::NAME,
-                Application::VERSION
-            );
+            return new SymfonyApplication($this->name, $this->version);
         });
         $app->bind(Dispatcher::class)->after(function (Dispatcher $dispatcher) {
             foreach ($this->commands as $name => $command) {
