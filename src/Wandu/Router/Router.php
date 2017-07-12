27107b13de09c6448ee5372b58b745c5eedbec01
Changwan Jun <im@wani.kr>
2017-07-12 17:13:11 +0900

    refactoring Router

--- src/Wandu/Router/Router.php
+++ src/Wandu/Router/Router.php
@@ -5,30 +5,27 @@ use IteratorAggregate;
 use Wandu\Router\Contracts\Route as RouteInterface;
 use Wandu\Router\Contracts\Router as RouterInterface;
 
-class Router implements IteratorAggregate, RouterInterface 
+class Router implements RouterInterface, IteratorAggregate 
 {
+    /** @var \Wandu\Router\Router[] */
+    protected $routers = [];
+
     /** @var \Wandu\Router\Route[] */
     protected $routes = [];
 
-    /** @var string */
-    protected $domains = ['@'];
-    
-    /** @var string */
-    protected $prefix = '';
-
     /** @var array */
-    protected $middlewares = [];
+    protected $status = [
+        'domains' => ['@'],
+        'prefix' => '',
+        'middlewares' => [],
+    ];
 
     /**
-     * @param string $prefix
-     * @param callable $handler
+     * {@inheritdoc}
      */
     public function prefix(string $prefix, callable $handler)
     {
-        $beforePrefix = $this->prefix;
-        $this->prefix = "{$beforePrefix}/" . ($prefix ?? '');
-        call_user_func($handler, $this);
-        $this->prefix = $beforePrefix;
+        $this->group(['prefix' => $prefix, ], $handler);
     }
 
     /**
@@ -36,11 +33,7 @@ class Router implements IteratorAggregate, RouterInterface
      */
     public function domain($domain, callable $handler)
     {
-        $domains = array_filter((array) $domain);
-        $beforeDomains = $this->domains;
-        $this->domains = $domains;
-        call_user_func($handler, $this);
-        $this->domains = $beforeDomains;
+        $this->group(['domain' => $domain, ], $handler);
     }
 
     /**
@@ -48,28 +41,38 @@ class Router implements IteratorAggregate, RouterInterface
      */
     public function middleware($middleware, callable $handler)
     {
-        $middlewares = array_filter((array) $middleware);
-        $beforeMiddlewares = $this->middlewares;
-        $this->middlewares = array_merge($beforeMiddlewares, $middlewares);
-        call_user_func($handler, $this);
-        $this->middlewares = $beforeMiddlewares;
+        $this->group(['middleware' => $middleware, ], $handler);
     }
 
     /**
-     * @param array $attributes
-     * @param callable $handler
+     * {@inheritdoc}
      */
     public function group(array $attributes, callable $handler)
     {
-        $this->prefix($attributes['prefix'] ?? '', function () use ($attributes, $handler) {
-            $this->middleware($attributes['middleware'] ?? [], function () use ($attributes, $handler) {
-                call_user_func($handler, $this);
-            });
-        });
+        $router = new Router();
+        $status = $this->status;
+        if (isset($attributes['prefix'])) {
+            $status['prefix'] = "{$status['prefix']}/" . $attributes['prefix'];
+        }
+        if (isset($attributes['middleware'])) {
+            $status['middlewares'] = array_merge($status['middlewares'], array_filter((array)$attributes['middleware']));
+        }
+        if (isset($attributes['middlewares'])) {
+            $status['middlewares'] = array_merge($status['middlewares'], array_filter((array)$attributes['middlewares']));
+        }
+        if (isset($attributes['domain'])) {
+            $status['domains'] = (array) $attributes['domain'];
+        }
+        if (isset($attributes['domains'])) {
+            $status['domains'] = (array) $attributes['domains'];
+        }
+        $router->status = $status;
+        call_user_func($handler, $router);
+        $this->routers[] = $router;
     }
 
     /**
-     * @param callable $handler
+     * {@inheritdoc}
      */
     public function append(callable $handler)
     {
@@ -79,11 +82,12 @@ class Router implements IteratorAggregate, RouterInterface
     public function resource($className, $except = [], $only = [])
     {
         $this->get('', $className, 'index');
-        $this->get('create', $className, 'create');
         $this->post('', $className, 'store');
-        $this->get('{id}', $className, 'show');
-        $this->put('{id}', $className, 'update');
-        $this->delete('{id}', $className, 'destroy');
+        $this->get(':id', $className, 'show');
+        $this->put(':id', $className, 'update');
+        $this->patch(':id', $className, 'patch');
+        $this->delete(':id', $className, 'destroy');
+        // $this->options();
     }
 
     /**
@@ -155,12 +159,12 @@ class Router implements IteratorAggregate, RouterInterface
      */
     public function createRoute(array $methods, string $path, string $className, string $methodName = 'index'): RouteInterface
     {
-        $path = trim("{$this->prefix}/{$path}", '/');
+        $path = trim("{$this->status['prefix']}/{$path}", '/');
         while(strpos($path, '//') !== false) {
             $path = str_replace('//', '/', $path);
         }
         $path = '/' . $path;
-        $route = new Route($className, $methodName, $this->middlewares, $this->domains);
+        $route = new Route($className, $methodName, $this->status['middlewares'], $this->status['domains']);
         $this->routes[] = [$methods, $path, $route];
         return $route;
     }
@@ -172,6 +176,11 @@ class Router implements IteratorAggregate, RouterInterface
     {
         foreach ($this->routes as $route) {
             yield $route;
+        }
+        foreach ($this->routers as $router) {
+            foreach ($router as $route) {
+                yield $route;
+            }
         }
     }
 }
