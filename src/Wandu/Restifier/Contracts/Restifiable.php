458bfdfcc0bb85cfc4d3e6edb268477afc0366ab
Changwan Jun <im@wani.kr>
2017-08-16 10:35:15 +0900

    add $transformer to Restifiable Contract @wandu/restifier

--- src/Wandu/Restifier/Contracts/Restifiable.php
+++ src/Wandu/Restifier/Contracts/Restifiable.php
@@ -6,14 +6,16 @@ interface Restifiable
     /**
      * @param mixed $resource
      * @param array $includes
+     * @param callable $transformer
      * @return array|null
      */
-    public function restify($resource, array $includes = []);
+    public function restify($resource, array $includes = [], callable $transformer = null);
     
     /**
      * @param array|\Traversable $resources
      * @param array $includes
+     * @param callable $transformer
      * @return array
      */
-    public function restifyMany($resources, array $includes): array;
+    public function restifyMany($resources, array $includes = [], callable $transformer = null): array;
 }
